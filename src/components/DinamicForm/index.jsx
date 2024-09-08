7855bf02b76fd432799cb11333156d289e599428
PsOlga <psyrniko1@gmail.com>
2024-09-08 19:41:29 +0200

    the work ist completed

--- src/components/DinamicForm/index.jsx
+++ src/components/DinamicForm/index.jsx
@@ -1,5 +1,4 @@
 
-
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import styles from "./style.module.css";
@@ -39,60 +38,60 @@ function DinamicForm() {
 
   return (
     <div className={styles.div_dinamic_form}>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.dinamic_Form}>
-      {/* Поле для логина */}
-      <label htmlFor="Login" className={styles.lable_login} >
-        <span >Login: </span>
-        <input className={styles.input_login}
-          {...register("Login", {
-            required: {
-              value: true,
-              message: "Это поле обязательно для заполнения",
-            },
-            minLength: {
-              value: 3,
-              message: "Логин не должен быть меньше 3 символов",
-            },
-            maxLength: {
-              value: 20,
-              message: "Длина логина не должна быть больше 20 символов",
-            },
-          })}
-          type="text"
-          id="Login"
-          onBlur={() => trigger("Login")} // Проверка валидации при потере фокуса
-        />
-      </label>
-
-      {/* Сообщения об ошибках для поля "Login" */}
-      {errors.Login && <p className={styles.error_message}>{errors.Login.message}</p>}
-
-      {/* Поле для пароля отображается только после успешной валидации логина */}
-      {loginValue?.length >= 3 && loginValue?.length <= 20 && !errors.Login && (
-        <label htmlFor="Password" className={styles.label_pas}>
-          <span>Password: </span>
-          <input
-            {...register("Password", {
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.dinamic_Form}>
+        {/* Поле для логина */}
+        <label htmlFor="Login" className={styles.lable_login} >
+          <span >Login: </span>
+          <input className={styles.input_login}
+            {...register("Login", {
               required: {
                 value: true,
                 message: "Это поле обязательно для заполнения",
               },
               minLength: {
-                value: 8,
-                message: "Пароль должен быть не менее 8 символов",
+                value: 3,
+                message: "Логин не должен быть меньше 3 символов",
+              },
+              maxLength: {
+                value: 20,
+                message: "Длина логина не должна быть больше 20 символов",
               },
             })}
-            type="password"
-            id="Password"
-            className={styles.input_pas}
+            type="text"
+            id="Login"
+            onBlur={() => trigger("Login")} // Проверка валидации при потере фокуса
           />
         </label>
-      )}
 
-      {errors.Password && <p className={styles.error_message_pas}>{errors.Password.message}</p>}
+        {/* Сообщения об ошибках для поля "Login" */}
+        {errors.Login && <p className={styles.error_message}>{errors.Login.message}</p>}
 
-      <button type="submit" className={styles.btn_dinamic_form}>Submit</button>
-    </form>
+        {/* Поле для пароля отображается только после успешной валидации логина */}
+        {loginValue?.length >= 3 && loginValue?.length <= 20 && !errors.Login && (
+          <label htmlFor="Password" className={styles.label_pas}>
+            <span>Password: </span>
+            <input
+              {...register("Password", {
+                required: {
+                  value: true,
+                  message: "Это поле обязательно для заполнения",
+                },
+                minLength: {
+                  value: 8,
+                  message: "Пароль должен быть не менее 8 символов",
+                },
+              })}
+              type="password"
+              id="Password"
+              className={styles.input_pas}
+            />
+          </label>
+        )}
+
+        {errors.Password && <p className={styles.error_message_pas}>{errors.Password.message}</p>}
+
+        <button type="submit" className={styles.btn_dinamic_form}>Submit</button>
+      </form>
     </div>
   );
 }
@@ -108,12 +107,12 @@ export default DinamicForm;
 //  const[validationMessege, setValidationMessege] = useState('');
 //  return (
 //     <form>
-//         <input 
+//         <input
 //         type="text"
 //         value={name}
 //         onChange={(event) => setName(event.target.value)}
 //         />
-//         <input 
+//         <input
 //         type="text"
 //         value={email}
 //         onChange={(event) => setEmail(event.target.value)}
